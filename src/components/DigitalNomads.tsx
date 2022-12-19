f552ef21cac812cef7e6660a01cb8ced5fe4b9e4
Andi <1vandidroid1@gmail.com>
2022-12-20 00:17:03 +0100

    Add sorting and filtering feature to the DigitalNomads component

--- src/components/DigitalNomads.tsx
+++ src/components/DigitalNomads.tsx
@@ -1,17 +1,99 @@
 import { useEffect, useState } from "react";
-import { DigitalNomadType } from "../types";
+import { DigitalNomadType, SortDirection, SortType } from "../types";
 import DigitalNomad from "./DigitalNomad";
 import "./DigitalNomads.scss";
 
 function DigitalNomads() {
-  const title = "Dinos";
-  const [digitalNomads, setDigitalNomads] = useState([]);
+  const title = "Digital Nomads";
+
+  const [digitalNomads, setDigitalNomads] = useState<DigitalNomadType[]>([]);
+  const [filteredDigitalNomads, setFilteredDigitalNomads] = useState<
+    DigitalNomadType[]
+  >([]);
+
+  const [nameFilter, setNameFilter] = useState("");
+
+  const [sort, setSort] = useState<SortType>({
+    criteria: "nickname",
+    direction: SortDirection.ASC,
+  });
 
   const fetchDigitalNomads = async () => {
     const result = await fetch("/api/nomads");
     const digitalNomads = await result.json();
     setDigitalNomads(digitalNomads);
+    setFilteredDigitalNomads(digitalNomads);
   };
+
+  const filterDigitalNomads = (e: React.FormEvent<HTMLInputElement>) => {
+    setNameFilter(e.currentTarget.value);
+  };
+
+  const digitalNomadComparator = (
+    digitalNomad1: DigitalNomadType,
+    digitalNomad2: DigitalNomadType
+  ) => {
+    switch (sort.criteria) {
+      case "nickname":
+        return (
+          sort.direction *
+          digitalNomad1.nickname.localeCompare(digitalNomad2.nickname)
+        );
+      case "firstName":
+        return (
+          sort.direction *
+          digitalNomad1.firstName.localeCompare(digitalNomad2.firstName)
+        );
+      case "lastName":
+        return (
+          sort.direction *
+          digitalNomad1.lastName.localeCompare(digitalNomad2.lastName)
+        );
+      case "email":
+        return (
+          sort.direction *
+          digitalNomad1.email.localeCompare(digitalNomad2.email)
+        );
+      case "locationName":
+        return (
+          sort.direction *
+          digitalNomad1.location.name.localeCompare(digitalNomad2.location.name)
+        );
+      case "countryName":
+        return (
+          sort.direction *
+          digitalNomad1.location.country.name.localeCompare(
+            digitalNomad2.location.country.name
+          )
+        );
+      case "gender":
+        return (
+          sort.direction *
+          digitalNomad1.gender.localeCompare(digitalNomad2.gender)
+        );
+
+      default:
+        return digitalNomad1.nickname.localeCompare(digitalNomad2.nickname);
+    }
+  };
+  const changeSortCriteria = (e: React.FormEvent<HTMLSelectElement>) => {
+    setSort({ ...sort, criteria: e.currentTarget.value });
+  };
+
+  const changeSortDirection = (e: React.FormEvent<HTMLSelectElement>) => {
+    setSort({
+      ...sort,
+      direction: Number(e.currentTarget.value),
+    });
+  };
+
+  useEffect(() => {
+    setFilteredDigitalNomads(
+      digitalNomads.filter((digitalNomad: DigitalNomadType) =>
+        digitalNomad.nickname.toLowerCase().includes(nameFilter.toLowerCase())
+      )
+    );
+  }, [nameFilter]);
 
   useEffect(() => {
     fetchDigitalNomads();
@@ -20,12 +102,51 @@ function DigitalNomads() {
   return (
     <div className="DigitalNomads">
       <h3 hidden>{title}</h3>
+
+      <section>
+        <input
+          type="search"
+          placeholder="Filter digital nomads"
+          value={nameFilter}
+          onInput={filterDigitalNomads}
+        />
+      </section>
+      <section className="flex-container">
+        <div>
+          {filteredDigitalNomads.length} of {digitalNomads.length} Nomads
+        </div>
+        <div>
+          <span>
+            <label>Sort by</label>
+          </span>
+          <span>
+            <select onChange={changeSortCriteria}>
+              <option value="nickname">nickname</option>
+              <option value="firstName">first name</option>
+              <option value="lastName">last name</option>
+              <option value="gender">gender</option>
+              <option value="email">email</option>
+              <option value="locationName">location name</option>
+              <option value="countryName">country name</option>
+            </select>
+          </span>
+          <span>
+            <select onChange={changeSortDirection}>
+              <option value={SortDirection.ASC}>asc</option>
+              <option value={SortDirection.DESC}>desc</option>
+            </select>
+          </span>
+        </div>
+      </section>
+
       <article>
-        {digitalNomads.map((digitalNomad: DigitalNomadType, index: number) => (
-          <div key={digitalNomad.id}>
-            <DigitalNomad index={index} data={digitalNomad} />
-          </div>
-        ))}
+        {filteredDigitalNomads
+          .sort(digitalNomadComparator)
+          .map((digitalNomad: DigitalNomadType, index: number) => (
+            <div key={digitalNomad.id}>
+              <DigitalNomad index={index} data={digitalNomad} />
+            </div>
+          ))}
       </article>
     </div>
   );
