4686af3e82a6ab8b5272e53f60d6b3b29ed14262
Andi <1vandidroid1@gmail.com>
2023-05-11 23:07:33 +0200

    Fix the global.ts

--- src/globals.ts
+++ src/globals.ts
@@ -1,3 +1,3 @@
-export const API_SERVER = "";
-//export const API_SERVER = "http://local.dinodev.hu";
+// export const API_SERVER = "";
+export const API_SERVER = "http://local.dinodev.hu";
 // export const API_SERVER = "https://api.dinodev.hu";
